c1490562e69ab2f27893ac50e760ddb7fb50f1bb
cauriflores <34865937+cauriflores@users.noreply.github.com>
2026-09-05 20:53:00 +0200

    Publish Pacheco web at /pacheco/
    Built from the private pacheco-web repo at 5629a7c.

--- pacheco/chunk-Bo3jFX-H.js
+++ pacheco/chunk-Bo3jFX-H.js
@@ -1,4 +1,4 @@
-import{A as Zv,B as ic,E as Wf,S as Sv,W as oc,Y as qv,f as Ie,nt as y$1,o as DE,tt as ty,x as Sp,y as Qv}from"./main-7R4Z4V37.js";function b(a,e){if(a&1&&(oc(0,`p`),DE(1),ic()),a&2){let t=e.$implicit;ty(),Wf(t)}}var y=`The expression "Lleg\xF3 Pacheco" (Pacheco arrived) originated in Venezuela and it is used to refer to the arrival of "cold" weather, specifically in December and in the city of Caracas.
+import{A as Zv,B as ic,E as Wf,S as Sv,W as oc,Y as qv,f as Ie,nt as y$1,o as DE,tt as ty,x as Sp,y as Qv}from"./main-FOFUP2TL.js";function b(a,e){if(a&1&&(oc(0,`p`),DE(1),ic()),a&2){let t=e.$implicit;ty(),Wf(t)}}var y=`The expression "Lleg\xF3 Pacheco" (Pacheco arrived) originated in Venezuela and it is used to refer to the arrival of "cold" weather, specifically in December and in the city of Caracas.
 
 The legend tells that a flower farmer named Antonio Pacheco came down to Caracas from Galip\xE1n, a small town on the El \xC1vila hill, at the end of November or beginning of December to sell his flowers.
 
