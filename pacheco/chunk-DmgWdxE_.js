60671e96718b5978207fb214c8501d7525c8ee2e
cauriflores <34865937+cauriflores@users.noreply.github.com>
2026-09-07 21:38:50 +0200

    Publish Pacheco web at /pacheco/
    Built from the private pacheco-web repo at 38d3a3a.

---
+++ pacheco/chunk-DmgWdxE_.js
@@ -0,0 +1,10 @@
+import{$ as rE,C as Up,F as dy,H as ic,J as np,Y as oE,d as Ie,it as tE,nt as sc,ot as y$1,r as AE,w as Vv}from"./main-P745N52C.js";function b(a,e){if(a&1&&(ic(0,`p`),AE(1),sc()),a&2){let t=e.$implicit;dy(),np(t)}}var y=`The expression "Lleg\xF3 Pacheco" (Pacheco arrived) originated in Venezuela and it is used to refer to the arrival of "cold" weather, specifically in December and in the city of Caracas.
+
+The legend tells that a flower farmer named Antonio Pacheco came down to Caracas from Galip\xE1n, a small town on the El \xC1vila hill, at the end of November or beginning of December to sell his flowers.
+
+His arrival coincided with the onset of cold season, which is why he began to be associated with this time of year. Pacheco came down three times a week, accompanied by his donkeys and the load of flowers. He sold them in front of the La Pastora church and then went to the San Jos\xE9 Flower Market.
+
+This tradition has been maintained for many years and became a part of Venezuelan folklore. Nowadays "Lleg\xF3 Pacheco" is still used to refer to the cold and the arrival of Christmas.
+
+It is common in Caracas to hear phrases like "Esta haciendo Pacheco" (It's getting cold) or "El Pacheco est\xE1 fuerte este a\xF1o." (This year the cold is strong)`;var g=class a{i18n=y$1(Up);paragraphs=Ie(()=>this.i18n.t(y).split(`
+`).map(e=>e.trim()).filter(e=>e!==``));static ɵfac=function(t){return new(t||a)};static ɵcmp=Vv({type:a,selectors:[[`app-about`]],decls:4,vars:1,template:function(t,c){t&1&&(ic(0,`h1`),AE(1),sc(),rE(2,b,2,1,`p`,null,tE)),t&2&&(dy(),np(c.i18n.t(`About Pacheco`)),dy(),oE(c.paragraphs()))},styles:[`p[_ngcontent-%COMP%]{margin:0 0 1rem;max-width:42rem;line-height:1.55}`]})};export{g as About};
